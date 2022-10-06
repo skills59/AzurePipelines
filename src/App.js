2bf1d423b97e5fbd8a9f09222fb74b848df7dcea
https://github.com/skills59/CICD-React-Pipeline.git <olatundeewuola@gmail.com>
2022-10-06 21:17:02 +0100

    app js update

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>
-        This is just a simple React JS application demo for an 
+        This project is just a simple React JS application demo for an 
           AWS Pipeline project Contineous Integration and Deployment CI/CD
         </h1>
         <p>By Olatunde Ajibayo Ewuola</p>
